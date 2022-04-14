14444632d988ef831f1f92fa4a9674f62c7b6f5b
Nicholas Hussain <nicholashussain@Nicholass-MacBook-Air-2.local>
2022-04-14 12:24:35 -0400

    Schema's for each collection

---
+++ dbModels/ reservation.js
@@ -0,0 +1,27 @@
+// reservation schema
+var mongoose = require("mongoose");
+const userSchema = require("./user").schema;
+const restSchema = require("./restaurant").schema;
+const tableSchema = require("./table").schema;
+
+const reserveSchema = new mongoose.Schema({
+   
+    userID: {
+        required:true,
+        type: userSchema
+    },
+    restID: {
+        required:true,
+        type: restSchema
+    },
+    tableID:{
+        required:true,
+        type: tableSchema
+    },
+    date: Date,
+    time: Number
+});
+const Reservation = mongoose.model('Reservation',reserveSchema);
+
+module.exports.model = Reservation;
+module.exports.schema = reserveSchema;
---
+++ dbModels/ReadMe.md
@@ -0,0 +1,7 @@
+The information and the idea to have each Schema model in the database came from:
+https://github.com/athoutam1/Restaurant-Reservation/tree/master/api/models
+
+Since the data is different in our schemas this was a good base to use in the creation of the mongoose schema.
+
+
+Models made by: Nicholas Hussain
