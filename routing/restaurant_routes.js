45f9951d2c66bbdb7736d8d7978647cb56708acf
fnemeth10 <fnemeth15@icloud.com>
2022-04-18 16:45:26 -0400

    Update restaurant_routes.js

--- routing/restaurant_routes.js
+++ routing/restaurant_routes.js
@@ -1,3 +1,10 @@
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+
+var Reservation = require("../dbModels/reservation").model;
+
+  
+
+model.exports = router;
