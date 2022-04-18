bdb447493ce198041b434204661c9105646fe292
fnemeth10 <fnemeth15@icloud.com>
2022-04-18 17:53:08 -0400

    Update table_routes.js

--- routing/table_routes.js
+++ routing/table_routes.js
@@ -1,3 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+var Reservation = require("../dbModels/reservation").model;
+var User = require("../dbModels/user").model;
+var Table = require("../dbModels/table").model;
+
+module.exports = router;
