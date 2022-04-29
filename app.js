4b3abb2bd5ab37583ddb3120d2933c1c14b5a7d2
Nicholas Hussain <nicholashussain@Nicholass-MacBook-Air-2.local>
2022-04-29 09:32:51 -0400

    routes in app

--- app.js
+++ app.js
@@ -3,22 +3,49 @@
 // Also inspired by: https://github.com/profjake/lecture15 
 // And inspired by: https://github.com/codrkai/node_react_mongodb_tutorial 
 
+
 let express = require('express')
 // let app = require('../Restaurant-Reservation/api/app')
 const mongoose = require('mongoose')
 const app = express();
 const dbManager = require("./dbTestingWork/dbManager")
 
+let bp = require('body-parser');
+let session = require('express-session');
+let crypto = require('crypto');
+function genHash(input){
+    return Buffer.from(crypto.createHash('sha256').update(input).digest('base32')).toString('hex').toUpperCase();
+}
 
-app.use('/routing', require('./routing/login'))
+app.use(session({
+	secret:'shhhhh',
+	saveUninitialized: false,
+	resave: false
+}));
+app.get("/",function(res,req){
+    if (!req.session.user){
+        res.redirect('/login');
+    }
+    else{
+    	res.render('NavBar', {trusted: req.session.user});
+	}
+});
+
+app.use('/login', require('./routing/login'))
+app.use('/AskingInfo', require("./routing/make_reservation"))
+app.use('/ManageRes',require("./routing/manage_reservation"))
+app.use('/CreatingAccount',require("./routing/sign_up"))
+
 
 mongoose.Promise = global.Promise
-mongoose.connect("mongodb://localhost:27017/", {useNewUrlParser: true, useUnifiedTopology: true})
+//mongoose.connect("mongodb://localhost:27017/", {useNewUrlParser: true, useUnifiedTopology: true})
 // mongoose.connection.on('error', () => {
 //     throw new Error(`unable to connect to database: `)
 // })
 
 
 app.listen(3000, (err) => {
+    mongoose.connect("mongodb://localhost:27017/", {useNewUrlParser: true, useUnifiedTopology: true})
+    console.log("Server is up and running");
     if (err) {console.log(err)}
 });
